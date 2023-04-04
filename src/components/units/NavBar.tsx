b8449e021cb5aa2806e5552ca1680d431ae58fc5
BruhovaAlena <bruhovaalena@gmail.com>
2023-04-04 19:56:51 +0200

    add navbar

--- src/components/units/NavBar.tsx
+++ src/components/units/NavBar.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Flex, Stack, useDisclosure, Icon, Text } from '@chakra-ui/react';
+import {
+  Flex,
+  Stack,
+  useDisclosure,
+  Icon,
+  Text,
+  theme,
+} from '@chakra-ui/react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 const NavBar = () => {
@@ -11,47 +18,89 @@ const NavBar = () => {
       px={4}
       direction={['column', 'column', 'row']}
       justifyContent="space-between"
-      bgColor="lightBlue"
-      position="fixed"
+      bgColor="white"
+      position="sticky"
       top="0"
       width="100%"
       overflow={'hidden'}
+      zIndex={1}
     >
       <Flex alignItems="center" wrap="wrap">
-        <Flex flexGrow={1} justify="center">
-          <Text ml={[4, 0]} fontSize="3xl" color="darkBlue">
-            Alenka
+        {/* <Flex flexGrow={1} justify="center">
+          <Text
+            ml={[4, 0]}
+            fontSize="3xl"
+            color="black"
+            fontFamily={theme.fonts.body}
+          >
+            Alena
           </Text>
-        </Flex>
+        </Flex> */}
         <Icon
           as={GiHamburgerMenu}
           onClick={isOpen ? onClose : onOpen}
           display={['inline', 'inline', 'none']}
           w="40px"
           h="40px"
-          color="darkBlue"
+          color="black"
         />
       </Flex>
       <Flex
         display={[isOpen ? 'flex' : 'none', isOpen ? 'flex' : 'none', 'flex']}
       >
         <Stack
-          direction={{ sm: 'column', md: 'row' }}
+          direction={{ base: 'column', md: 'row' }}
           align={{ sm: 'left', md: 'center' }}
+          spacing="30px"
         >
-          <a href="#1" style={{ color: '#3E54AC', fontSize: '20px' }}>
+          <a
+            href="#home"
+            style={{
+              color: '#000000',
+              fontSize: '20px',
+              fontFamily: theme.fonts.body,
+            }}
+          >
             Home
           </a>
-          <a href="#2" style={{ color: '#3E54AC', fontSize: '20px' }}>
+          <a
+            href="#skills"
+            style={{
+              color: '#000000',
+              fontSize: '20px',
+              fontFamily: theme.fonts.body,
+            }}
+          >
             Skills
           </a>
-          <a href="#3" style={{ color: '#3E54AC', fontSize: '20px' }}>
+          <a
+            href="#about"
+            style={{
+              color: '#000000',
+              fontSize: '20px',
+              fontFamily: theme.fonts.body,
+            }}
+          >
             About me
           </a>
-          <a href="#4" style={{ color: '#3E54AC', fontSize: '20px' }}>
+          <a
+            href="#projects"
+            style={{
+              color: '#000000',
+              fontSize: '20px',
+              fontFamily: theme.fonts.body,
+            }}
+          >
             Projects
           </a>
-          <a href="#5" style={{ color: '#3E54AC', fontSize: '20px' }}>
+          <a
+            href="#contacts"
+            style={{
+              color: '#000000',
+              fontSize: '20px',
+              fontFamily: theme.fonts.body,
+            }}
+          >
             Contacts
           </a>
         </Stack>
